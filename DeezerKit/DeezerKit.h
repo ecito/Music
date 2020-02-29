c31f707262c06b59e4c404eb678f0d84994be9e4
Andre Navarro <andre.navarro@gmail.com>
2020-02-29 16:32:19 +0100

    project set up with DeezerKit framework

---
+++ DeezerKit/DeezerKit.h
@@ -0,0 +1,19 @@
+//
+//  DeezerKit.h
+//  DeezerKit
+//
+//  Created by Andre Navarro on 2/28/20.
+//  Copyright © 2020 DML. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for DeezerKit.
+FOUNDATION_EXPORT double DeezerKitVersionNumber;
+
+//! Project version string for DeezerKit.
+FOUNDATION_EXPORT const unsigned char DeezerKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <DeezerKit/PublicHeader.h>
+
+

c8aa6a096cd2f38931bb77f6dacb975c1c65b48f
Andre Navarro <andre.navarro@gmail.com>
2020-03-02 00:17:48 +0100

    dependency injection, because of course

--- Music/Dependencies.swift
+++ Music/Dependencies.swift
@@ -10,7 +10,19 @@ import Foundation
 import DeezerKit
 import NetworkKit
 
-struct Dependencies {
+/// Union of protocols that different parts of the app can depend on
+typealias AllDependencies = HasNetwork & HasDeezerService
+
+/// All the app's dependencies in one place
+class AppDependencies: AllDependencies {
     lazy var network: NetworkType = Network()
     lazy var deezerService = DeezerService(network: network)
+}
+
+protocol HasNetwork {
+    var network: NetworkType { get }
+}
+
+protocol HasDeezerService {
+    var deezerService: DeezerService { get }
 }
